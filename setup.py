df1a8dd3f1c9264d383d796b07fffb3a9704c0ec
Erik Telepovský <erik.telepovsky@gmail.com>
2020-04-01 14:52:20 +0200

    v2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-invoicing',
-    version='2.1.1',
+    version='2.2.0',
     description='Django app for invoicing.',
     long_description=open('README.rst').read(),
     author='Pragmatic Mates',

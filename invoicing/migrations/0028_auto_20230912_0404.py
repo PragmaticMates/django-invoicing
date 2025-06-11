810dd10633d954ba51a37f77af89928835325cee
Karina Marcineková <karina.marcinekova@swida.sk>
2025-06-11 12:56:52 +0200

    migration: model name - uppercase letter

--- invoicing/migrations/0028_auto_20230912_0404.py
+++ invoicing/migrations/0028_auto_20230912_0404.py
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='invoice',
             name='related_invoices',
-            field=models.ManyToManyField(blank=True, to='invoicing.invoice'),
+            field=models.ManyToManyField(blank=True, to='invoicing.Invoice'),
         ),
         migrations.AlterField(
             model_name='invoice',
